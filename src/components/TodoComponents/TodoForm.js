c26357dda527818ea70b209e80556d658e9683a9
cwern40 <chris.wernli@hotmail.com>
2019-06-05 19:08:14 -0600

    Checking to see if the form works with the onSubmit on the <form> or onClick on the <button>

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const TodoForm = props => {
     return (
-        <form>
+        <form onSubmit={props.addTodo}>
             <input type="text" placeholder="enter your task" name="todo" className="todo-input" value={props.value} onChange={props.changeTodo}/>
-            <button onClick={props.addTodo}>Add todo</button>
+            <button>Add todo</button>
             <button>Clear Completed</button>
         </form>
     );
